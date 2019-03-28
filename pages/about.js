4e014ec55aaf575e6e9d157e4529377c7c840efe
Jason Wiesinger <jawiesinger@gmail.com>
2019-03-28 16:22:45 -0500

    First commit

---
+++ pages/about.js
@@ -0,0 +1,31 @@
+import Layout from "../components/Layout";
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About BitzPrice</h1>
+      <p>Application to view Bitcoin prices</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+        tristique ipsum eu semper ultricies. In vel lorem eu mauris convallis
+        rhoncus sit amet vel nisl. Aenean imperdiet sed diam vel mollis.
+        Phasellus congue eleifend euismod. Donec porttitor luctus velit vel
+        scelerisque. Cras sed tincidunt enim. Nam tristique vehicula congue. In
+        vulputate erat id orci finibus tincidunt. Nullam eget nibh sapien.
+      </p>
+      <p>
+        Vivamus at tincidunt nunc. Vivamus condimentum fringilla fringilla. Sed
+        fringilla purus sed ex consectetur mattis. Fusce vitae risus fringilla,
+        bibendum velit ut, varius ante. Etiam eget nulla elementum, interdum
+        purus sit amet, fermentum est. Praesent eget scelerisque lectus, sit
+        amet hendrerit orci. Vivamus erat mauris, egestas quis luctus et,
+        viverra at risus. Ut lorem ipsum, dictum at rhoncus vitae, rhoncus nec
+        velit. Donec vitae iaculis neque. In vel suscipit justo. Fusce non
+        sollicitudin nulla. Sed lobortis malesuada lacus. Praesent arcu sapien,
+        elementum nec eros sit amet, consectetur congue eros. Cras mollis
+        eleifend elit, non dignissim turpis volutpat vel.
+      </p>
+    </div>
+  </Layout>
+);
+export default About;
